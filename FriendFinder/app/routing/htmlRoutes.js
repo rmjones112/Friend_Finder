9b46f6ee626128b9af3df7cd40546cc5f5f391f6
rmjones112 <rmjones112gmail.com>
2019-10-06 16:20:18 -0700

    bringing up to date

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -1,1 +1,27 @@
+var path = require("path");
+var express = require('express');
+var htmlRouter = express.Router(); 
+
 //local navigation
+// module.exports = function(app){
+
+//     app.get('/', function(req, res){
+//         //takes some path and returns file back to requester
+//         res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
+//     });
+//     app.get('/survey', function(req, res){
+//         //takes some path and returns file back to requester
+//         res.sendFile(path.join(__dirname, '..', 'public', 'survey.html'));
+//     });
+// }
+
+htmlRouter.get('/', function(req, res){
+        //takes some path and returns file back to requester
+        res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
+    });
+    htmlRouter.get('/survey', function(req, res){
+        //takes some path and returns file back to requester
+        res.sendFile(path.join(__dirname, '..', 'public', 'survey.html'));
+    });
+
+    module.exports = htmlRouter;
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,3 +1,16 @@
 //setting dependencies
 //express is required what else is?
 var express = require("express");
+//
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+var htmlRouter = require('./app/routing/htmlRoutes')
+
+app.use(express.json());
+app.use(htmlRouter);
+
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+
+  });
